2d555274685599e2a61ca51e15ef006c76c73022
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-10 12:42:43 -0800

    Create data generation script

--- database/generateData.js
+++ database/generateData.js
@@ -1,5 +1,32 @@
 const fs = require('fs');
 const faker = require('faker');
+const path = require('path');
 const { createRandomReview } = require('./helpers.js');
 
-const generateRestaurantId = () => faker.random.number({ min: 1, max: 2000000 });
+const generateRestaurantId = () => faker.random.number({ min: 1, max: 50000 });
+const writeStream = fs.createWriteStream(path.join(__dirname, '/data/sampleData1.txt'));
+const numRecords = 1000000;
+let i = 0;
+
+const writeOneRecord = () => {
+  while (i < numRecords) {
+    const restId = generateRestaurantId();
+    const entry = {
+      id: i,
+      review: createRandomReview(restId),
+    };
+
+    if (!writeStream.write(JSON.stringify(entry) + '\n')) {
+      return;
+    }
+
+    i += 1;
+  }
+  writeStream.end();
+};
+
+writeStream.on('drain', () => {
+  writeOneRecord();
+});
+
+writeOneRecord();
--- database/helpers.js
+++ database/helpers.js
@@ -9,7 +9,8 @@ const generateNoiseLevel = () => faker.random.arrayElement(['do not recall', 'qu
 const generateRecommend = () => faker.random.boolean();
 const generateReviewContent = () => faker.lorem.paragraph();
 const generateHelpfulCount = () => faker.random.number({ min: 0, max: 3 });
-const generateRandomId = () => faker.random.number(999999);
+const generateRandomReviewerId = () => faker.random.number(5000000);
+const generateRandomReviewId = () => faker.random.number(50000000);
 const generateTags = () => {
   const tagsArr = [];
   let randNum = faker.random.number({ min: 0, max: 6 });
@@ -79,14 +80,14 @@ const createRandomReview = (restId) => {
       id: restId,
     },
     reviewer: {
-      id: generateRandomId(),
+      id: generateRandomReviewerId(),
       nickname: generateNickname(),
       location: generateLocation(),
       review_count: generateReviewCount(),
       date_dined: generateDateDined(),
     },
     review: {
-      id: generateRandomId(),
+      id: generateRandomReviewId(),
       ratings: {
         overall: generateRatings(),
         food: generateRatings(),
