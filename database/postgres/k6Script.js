6369878ab3fd0856223cc38a276413222ac54a24
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-19 12:01:56 -0800

    Update k6 script

--- database/postgres/k6Script.js
+++ database/postgres/k6Script.js
@@ -2,23 +2,19 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  // vus: 15,
+  vus: 100,
   rps: 1000,
-  // duration: '10s',
-  stages: [
-    { duration: "5m", target: 1 },
-    { duration: "5m", target: 10 },
-    { duration: "5m", target: 50 },
-    { duration: "5m", target: 100 },
-  ],
+  duration: '5m',
 };
 
 export default function() {
   const randomId = Math.floor(Math.random() * (10000000 - 9000000 + 1)) + 9000000;
-  const response = http.get(`http://localhost:3004/api/restaurants/${randomId}/reviews`);
+  // const response = http.get(`http://localhost:3004/api/restaurants/${randomId}/reviews`);    // reviews service
+  const response = http.post(`http://localhost:3004/api/restaurants/${randomId}/reviews`);      // reviews service
   check(response, {
-    "status was 200": (r) => r.status == 200,
+    // "status was 200": (r) => r.status == 200,    // for GET requests
+    "status was 201": (r) => r.status == 201,       // for POST requests
     "transaction time OK": (r) => r.timings.duration < 2000
   });
-  sleep(0.1);
+  // sleep(0.05);
 };
